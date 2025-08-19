5b8daf91cbc73b3932d9b0771f9fc982f0e6a50e
monika994453-creator <monika994453@gmail.com>
2025-08-19 10:19:08 +0530

    Add files via upload

---
+++ open cv/practise.py
@@ -0,0 +1,7 @@
+import cv2
+import matplotlib.pyplot as plt
+img=cv2.imread("1 Dakar.webp")
+print(img.shape)
+cv2.rectangle(img,(50,50),(100,100),(255,0,0),2)
+plt.imshow(cv2.cvtColor(img,cv2.COLOR_BGR2RGB))
+plt.show()
